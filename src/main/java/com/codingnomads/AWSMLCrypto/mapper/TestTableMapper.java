c8dbcc47ddb94fe4a0f59231071ca993d78ac6ee
Meg <the_meej@yahoo.com>
2018-01-02 10:28:53 -0500

    changed time to unixtime

--- src/main/java/com/codingnomads/AWSMLCrypto/mapper/TestTableMapper.java
+++ src/main/java/com/codingnomads/AWSMLCrypto/mapper/TestTableMapper.java
@@ -1,26 +1,22 @@
 package com.codingnomads.AWSMLCrypto.mapper;
 
-import com.amazonaws.services.machinelearning.model.PredictResult;
 import com.codingnomads.AWSMLCrypto.model.Data;
 import com.codingnomads.AWSMLCrypto.model.PredictCustomPojo;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
-import java.util.ArrayList;
-import java.util.Map;
-
 @Mapper
 public interface TestTableMapper {
 
-    public final String GET_TIME = "select time from data where time = #{time}";
-    public final String INSERT_DATA = "insert into data (closevalue, highvalue, lowvalue, openvalue, volumefrom, volumeto, time)" +
+    public final String GET_TIME = "select unixtime from data where unixtime = #{time}";
+    public final String INSERT_DATA = "insert into data (closevalue, highvalue, lowvalue, openvalue, volumefrom, volumeto, unixtime)" +
             "values (#{close}, #{high}, #{low}, #{open},#{volumeFrom}, #{volumeTo}, #{time})";
-    public final String SELECT_LATEST_TIME = "select time from data where time = (select max(time) from data)";
-    public final String SELECT_CLOSEVALUE_FROM_LATEST_ENTRY = "select closevalue from data where time = (select max(time) from data)";
+    public final String SELECT_LATEST_TIME = "select unixtime from data where unixtime = (select max(unixtime) from data)";
+    public final String SELECT_CLOSEVALUE_FROM_LATEST_ENTRY = "select closevalue from data where unixtime = (select max(unixtime) from data)";
     public final String INSERT_PREDICT_DATA = "insert into predictions (requestdate, amznrequestid, modeltype, " +
-            "highValuepredict, coinid, time) values (#{requestDate}, #{amznRequestId}, #{modelType}, " +
+            "highValuepredict, coinid, unixtime) values (#{requestDate}, #{amznRequestId}, #{modelType}, " +
             "#{highValuePredict}, #{coinId}, #{time})";
-    public final String GET_COINID = "select coinid from data where time = #{time}";
+    public final String GET_COINID = "select coinid from data where unixtime = #{time}";
 
     @Select(GET_TIME)
     public int getTime(int time);
