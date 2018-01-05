af775b9a84ea10cb11e0d8adf99e537551e0d44a
Meg <the_meej@yahoo.com>
2018-01-05 17:44:29 -0500

    working on creating models programatically

--- src/main/java/com/codingnomads/AWSMLCrypto/controller/PredictionController.java
+++ src/main/java/com/codingnomads/AWSMLCrypto/controller/PredictionController.java
@@ -4,6 +4,7 @@ import com.codingnomads.AWSMLCrypto.model.PredictCustomPojo;
 import com.codingnomads.AWSMLCrypto.service.PredictionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -18,8 +19,10 @@ public class PredictionController {
     PredictionService predictionService;
 
     @RequestMapping("/realtime")
-    public PredictCustomPojo realTimePrediction(){
-        return predictionService.getRealTimePrediction();
+    public PredictCustomPojo realTimePrediction(
+            @RequestParam(value = "createModel", required = true, defaultValue = "false") boolean createModel,
+            @RequestParam (value = "modelId", required = false) String modelId ){
+        return predictionService.getRealTimePrediction(createModel, modelId);
     }
 
     @RequestMapping("/analyze")
--- src/main/java/com/codingnomads/AWSMLCrypto/service/PredictionService.java
+++ src/main/java/com/codingnomads/AWSMLCrypto/service/PredictionService.java
@@ -1,5 +1,6 @@
 package com.codingnomads.AWSMLCrypto.service;
 
+import com.amazonaws.internal.SdkInternalMap;
 import com.amazonaws.services.machinelearning.AbstractAmazonMachineLearning;
 import com.amazonaws.services.machinelearning.AmazonMachineLearningClient;
 import com.amazonaws.services.machinelearning.model.*;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 import static java.lang.Math.abs;
 
@@ -22,56 +24,83 @@ public class PredictionService extends AbstractAmazonMachineLearning {
 
     @Autowired
     TestTableMapper mapper;
+    public boolean createModel = false;
+    public String modelId = null;
+    public String mlModelName = null;
 
     /**
-     * second iteration of the real time prediction method.. creating model and endpoint in aws console
+     * third iteration of the real time prediction method.. creating model & endpoint in program
      * saving results to redshift db (predictions table)
-     *
-     * @param
+     * @param createModel, modelId status is defined by the user via url query param, existing modelid must be provided to use
      */
-    public PredictCustomPojo getRealTimePrediction() {
+    // todo add model name as query param
+    public PredictCustomPojo getRealTimePrediction(boolean createModel, String modelId) {
         // get latest time (used to query for coinid)
         int latestTime = mapper.selectLatestTime();
         // add 1 hour (used to insert into prediction db)
         int predictionTimeInt = mapper.selectLatestTime() + 3600;
-        // convert to String (used in prediction request)
-        //String predictionTimeStr = String.valueOf(predictionTimeInt);
         // select previous hour's closevalue & give to AWS as next hour's openvalue
         String recordOpenValue = String.valueOf(mapper.selectCloseValueFromLatestEntry());
 
-        // create AWS prediction request
-        PredictRequest predictRequest = new PredictRequest()
-                // identify model to use (created programatically
-                .withMLModelId(createAwsMlModel().toString())   //("ml-AWCa7W6ltxQ") <-(created in the AWS console)
-                // identify prediction endpoint (also created in the AWS console)
-                .withPredictEndpoint("https://realtime.machinelearning.us-east-1.amazonaws.com")
-                // give AWS known data for prediction entry
+        PredictRequest predictRequest = new PredictRequest();
+        predictRequest
                 .addRecordEntry("time", String.valueOf(predictionTimeInt))
                 .addRecordEntry("openvalue", recordOpenValue);
+        if (createModel == false) {
+            // use an existing model, specified by modelId, which must exist in your aws account
+            // todo figure out a way to check that the model exists?
+            predictRequest.setMLModelId(modelId);
+            // specify endpoint (must be activated for real time predictions)
+            // todo get endpoint based on provided model id. could be param but it's a long string
+            predictRequest.setPredictEndpoint("https://realtime.machinelearning.us-east-1.amazonaws.com");
+        } else {
+            // programmatically create a new model
+            // todo make it possible to pass specific model parameters
+            CreateMLModelResult result = createAwsMlModel(modelId, mlModelName);
+            predictRequest.setMLModelId(result.getMLModelId());
 
-        // create AWS ML client
-        AmazonMachineLearningClient client = new AmazonMachineLearningClient();
-        // Use client to get the results of the prediction
-        PredictResult predictResult = client.predict(predictRequest);
+            // todo figure out how to make endpoint available programatically for a newly created model
+            predictRequest.setPredictEndpoint("https://realtime.machinelearning.us-east-1.amazonaws.com");
+        }
+        // status will likely be pending for a few minutes, if pending or ready then move on to get prediction
+        GetMLModelResult model = new GetMLModelResult();
+        // todo delete after testing:
+        model.getMLModelId();
+        // not sure if this can be done for an existing model. may need to put inside the above else block
 
-        // use custom results POJO to extract desired data points from predictResult
         PredictCustomPojo predictPojo = new PredictCustomPojo();
+        String modelStatus = model.getStatus();
 
-        predictPojo.setRequestDate(predictResult.getSdkHttpMetadata().getHttpHeaders().get("Date"));
-        predictPojo.setAmznRequestId(predictResult.getSdkResponseMetadata().getRequestId());
-        predictPojo.setHighValuePredict(predictResult.getPrediction().getPredictedValue());
-        predictPojo.setCoinId(mapper.getCoinId(latestTime));
-        predictPojo.setTime(predictionTimeInt);
-        predictPojo.setModelTypeId(mapper.getModelTypeId(predictResult.getPrediction().getDetails().get("PredictiveModelType")));
-        predictPojo.setAwsMLModelId(predictRequest.getMLModelId());
+        if ((modelStatus == "DELETED") || (modelStatus == "FAILED")){
+            System.out.println("Model status is " + modelStatus + ". It can't be used");
+        } else if ((modelStatus == "PENDING") || (modelStatus == "INPROGRESS")){
+            while (modelStatus != "COMPLETED"){
+                System.out.println("Model status is " + modelStatus + ". Checking again..");
+                modelStatus = model.getStatus();
+            }
+        } else if (modelStatus == "COMPLETED"){
+            // create AWS ML client
+            AmazonMachineLearningClient client = new AmazonMachineLearningClient();
+            // Use client to get the results of the prediction
+            PredictResult predictResult = client.predict(predictRequest);
+            // use custom results POJO to extract desired data points from predictResult
+            predictPojo.setRequestDate(predictResult.getSdkHttpMetadata().getHttpHeaders().get("Date"));
+            predictPojo.setAmznRequestId(predictResult.getSdkResponseMetadata().getRequestId());
+            predictPojo.setHighValuePredict(predictResult.getPrediction().getPredictedValue());
+            predictPojo.setCoinId(mapper.getCoinId(latestTime));
+            predictPojo.setTime(predictionTimeInt);
+            predictPojo.setModelTypeId(mapper.getModelTypeId(predictResult.getPrediction().getDetails().get("PredictiveModelType")));
+            predictPojo.setAwsMLModelId(predictRequest.getMLModelId());
 
-        // insert results in db
-        insertPredictResult(predictPojo);
+            // insert results in db
+            insertPredictResult(predictPojo);
+        }
+        // todo create an endpoint/ make it real time ready -- should have a check for existing endpoint first
         return predictPojo;
     }
 
     /**
-     * Method to insert prediction data in db
+     * Method that inserts prediction data in db
      * @param result
      */
     public void insertPredictResult(PredictCustomPojo result) {
@@ -113,15 +142,31 @@ public class PredictionService extends AbstractAmazonMachineLearning {
         // set the actual and percent error and then return the object so we see the whole record in json?
     }
 
-    public CreateMLModelResult createAwsMlModel(){
-
-        // Programatically create an ML Model, using existing training datasource
+    /**
+     * Method to programatically create a new AWS ML Model
+     * @param mlModelId
+     * @return
+     */
+    public CreateMLModelResult createAwsMlModel(String mlModelId, String mlModelName){
         CreateMLModelRequest mLModelRequest = new CreateMLModelRequest();
-        // user provided id & name
-        mLModelRequest.setMLModelId("ml-test-1");
-        mLModelRequest.setMLModelName("ML Model: Java Model Creation Test");
-        mLModelRequest.setMLModelType(MLModelType.REGRESSION);
+
+        if (mlModelId == null){
+            // generates unique identifier if model id is not provided
+            mlModelId = "ml-" + UUID.randomUUID().toString();   // todo could check that id doesn't already exist
+        }
+
+        if (mlModelName == null){
+            // generates unique name if model name is not provided
+            mlModelName = mlModelId + " Model";     // todo could check that name doesn't already exist
+        }
+
+        // user provides id & name
+        mLModelRequest.setMLModelId(mlModelId);
+        mLModelRequest.setMLModelName(mlModelName);   // todo make this a parameter (query param?)
+        mLModelRequest.setMLModelType(MLModelType.REGRESSION);  // todo not necessary but this could also be a param
+
         // hash map of parameters
+        // todo potentially make these query params too, so you can tweak models on the fly
         Map<String, String> parameters = new HashMap<String,String>();
         parameters.put("sgd.maxMLModelSizeInBytes", "13107200" );   // = 100 MB
         parameters.put("sgd.maxPasses", "50");
@@ -133,21 +178,14 @@ public class PredictionService extends AbstractAmazonMachineLearning {
         //mLModelRequest.setRecipe(); // AWS creates default if not provided
         mLModelRequest.setTrainingDataSourceId("ds-wW8vfMKT0qv");
 
-        // Create prediction request
         AmazonMachineLearningClient client = new AmazonMachineLearningClient();
-        // I'm expecting an error due to the time involved in creating models (15 mins)
+        // I'm expecting an error due to the time involved in creating models (2-3 mins)
         CreateMLModelResult result =  new CreateMLModelResult();
         try {
             result = client.createMLModel(mLModelRequest);
         } catch (Exception e){
-            System.out.println("Models take a few mins before you can use them. Check again in 10.");
+            System.out.println("Model creation takes a few minutes. Model ID:" + result.getMLModelId() + "will be ready for use soon.");
         }
-
-//        CreateDataSourceFromRedshiftRequest dataSourceRequest = new CreateDataSourceFromRedshiftRequest();
-//        dataSourceRequest.setComputeStatistics(true);
-//        dataSourceRequest.setDataSourceId("ds-000001");
-//        dataSourceRequest.setDataSourceName();
         return result;
     }
-
 }
