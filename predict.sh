98cd1257065a763e1ac3d3512b5b50f073d87f49
Meg <the_meej@yahoo.com>
2018-01-22 14:16:21 -0500

    scripts for analyze & predict

--- predict.sh
+++ predict.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-curl -i -H "Accept: application/json" -H "Content-Type: application/json" http://18-218-206-16:8080/predictionApiBot/predict/{realtime,analyze}
+createmodel = $1
+modelId = $2
+modelName = $3
+
+echo "/predictionApiBot/predict/realtime executing..."
+curl -i -H "Accept: application/json" -H "Content-Type: application/json" http://18.218.206.16:8080/predictionApiBot/
+predict/realtime?createmodel={$1}&modelId={$2}&modelName={$3}
+echo "\n Realtime Prediction complete"
