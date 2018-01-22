303b8cb10661fa73618ba6a462831b5aaded3a31
Meg <the_meej@yahoo.com>
2018-01-22 14:17:14 -0500

    changed return type of /analyze

--- src/main/java/com/codingnomads/AWSMLCrypto/controller/PredictionController.java
+++ src/main/java/com/codingnomads/AWSMLCrypto/controller/PredictionController.java
@@ -1,5 +1,6 @@
 package com.codingnomads.AWSMLCrypto.controller;
 
+import com.codingnomads.AWSMLCrypto.model.AnalyzeResult;
 import com.codingnomads.AWSMLCrypto.model.PredictCustomPojo;
 import com.codingnomads.AWSMLCrypto.service.PredictionService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,17 +18,18 @@ public class PredictionController {
 
     @Autowired
     PredictionService predictionService;
+    PredictCustomPojo predictCustomPojo;
 
     @RequestMapping("/realtime")
     public PredictCustomPojo realTimePrediction(
             @RequestParam(value = "createmodel", defaultValue = "false") boolean createModel,
-            @RequestParam (value = "modelId", required = false) String modelId,
+            @RequestParam(value = "modelId", required = false) String modelId,
             @RequestParam(value = "modelName", required = false) String modelName){
         return predictionService.getRealTimePrediction(createModel, modelId, modelName);
     }
 
     @RequestMapping("/analyze")
-    public void analyzeRealTimePrediction(){
-        predictionService.analyzePrediction();
+    public AnalyzeResult analyzeRealTimePrediction(){
+        return predictionService.analyzePrediction();
     }
 }
