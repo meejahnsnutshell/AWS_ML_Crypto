83a5f4be84f3885d4a5b074ca3028fd67a0714f9
Meg <the_meej@yahoo.com>
2018-01-23 20:37:12 -0500

    editing scripts to accept arguments

--- analyze.sh
+++ analyze.sh
@@ -2,4 +2,6 @@
 
 echo "/predictionApiBot/predict/analyze executing..."
 curl -i -H "Accept: application/json" -H "Content-Type: application/json" http://18.218.206.16:8080/predictionApiBot/predict/analyze
-echo "\n Analyze Prediction complete"
+echo ""
+echo "Analysis of Prediction complete"
+
--- predict.sh
+++ predict.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-createmodel = $1
-modelId = $2
-modelName = $3
-
 echo "/predictionApiBot/predict/realtime executing..."
-curl -i -H "Accept: application/json" -H "Content-Type: application/json" http://18.218.206.16:8080/predictionApiBot/
-predict/realtime?createmodel={$1}&modelId={$2}&modelName={$3}
-echo "\n Realtime Prediction complete"
+curl -i -H "Accept: application/json" -H "Content-Type: application/json" http://18.218.206.16:8080/predictionApiBot/predict/realtime -d createmodel=$1 -d modelId=$2 -d modelName=$3 -G
+echo ""
+echo "Realtime Prediction complete"
+
