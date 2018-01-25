22f102a2f6ebc2eb76b8f1bf1595cd411c78ae6d
Meg <the_meej@yahoo.com>
2018-01-25 11:59:39 -0500

    edited way args are received

--- predict.sh
+++ predict.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
+# This script assumes an existing aws model. Simply provide modelId and modelName when called.
+
 echo "/predictionApiBot/predict/realtime executing..."
-curl -i -H "Accept: application/json" -H "Content-Type: application/json" http://18.218.206.16:8080/predictionApiBot/predict/realtime -d createmodel=$1 -d modelId=$2 -d modelName=$3 -G
+curl -i -H "Accept: application/json" -H "Content-Type: application/json" http://18.218.206.16:8080/predictionApiBot/predict/realtime -d createmodel=false -d modelId=$1 -d modelName=$2 -G
 echo ""
 echo "Realtime Prediction complete"
-
